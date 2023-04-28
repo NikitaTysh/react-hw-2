cb14ffebf0c0a974ad48da418da08d9db480a71f
NikitaTysh <nikita.tyshchenko7@gmail.com>
2023-04-28 22:07:34 +0200

    update Friend list -> Item

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,11 @@
+
+import { ClassName } from "./test";
 export const App = () => {
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <button  >asddsa</button>
+     
     </div>
   );
 };
---
+++ src/components/test.js
@@ -0,0 +1,17 @@
+import React   from "react"
+
+export class ClassName extends React.Component
+{   
+    Foo = () =>{
+        console.log('ds');
+    }
+
+    render(){
+        return (
+            <div>
+            asd
+            </div>
+        )
+    }
+   
+}
